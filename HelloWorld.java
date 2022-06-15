279fadde13fd7f152198adb851c63deda44f9b54
Brandon-Soledad <brandonsoledad7@hotmail.com>
2022-06-14 22:52:53 -0700

    changed add value

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(addNumbers(3, 5));
+        System.out.println(addNumbers(5, 5));
 
     }
 
