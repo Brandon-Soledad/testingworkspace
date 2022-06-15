0b56162224b8dd31d4aae5720c5c23e4612e2a27
Brandon <brandonsoledad7@hotmail.com>
2022-06-14 22:02:12 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(addNumbers(3, 5));
+
+    }
+
+
+    public static int addNumbers(int a, int b) {
+      return a + b;
+    }
+
+
+}
